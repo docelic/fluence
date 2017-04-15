08a6645b1525fe07c038b5071301a66a24e0ddf8
Arthur Poulet <arthur.poulet@mailoo.org>
2017-04-15 14:31:07 +0100

    Use slang instead of ECR, improve verification

--- src/mdwikiface/web.cr
+++ src/mdwikiface/web.cr
@@ -1,6 +1,10 @@
+private def get_file_query(env)
+  env.request.resource.gsub(/^\/\w+\//, "")
+end
+
 private def get_file_path(env)
-  rpath = env.request.resource.gsub(/^\/\w+/, "")
-  gpath = "." + rpath + ".md"
+  rpath = get_file_query(env)
+  gpath = "./" + rpath + ".md"
   puts "File read: #{gpath}"
   gpath
 end
@@ -13,8 +17,8 @@ error 403 do
   "Forbidden"
 end
 
-before_all do |env|
-  next if env.request.resource == "/"
+private def verify_request(env)
+  return if env.request.resource == "/"
   file = get_file_path(env)
   begin
     rfile = File.real_path(file)
@@ -26,29 +30,44 @@ before_all do |env|
   end
 end
 
+VIEW_LAYOUT = "src/mdwikiface/views/layout.html.ecr"
+VIEW_NEW    = "src/mdwikiface/views/new.html.ecr"
+
 get "/" { |env| env.redirect "/show/README" }
 
 get "/new" do |env|
+  title = "New page"
+  current_path = nil
+  render "src/mdwikiface/views/new.html.slang", "src/mdwikiface/views/layout.html.slang"
 end
 
+before_get "/show/*" { |env| verify_request(env) }
+get "/show/*" do |env|
+  content = Markdown.to_html(File.read(get_file_path(env))) rescue env.response.status_code = 404
+  current_path = get_file_query(env)
+  title = File.basename current_path
+  render "src/mdwikiface/views/layout.html.ecr"
+end
+
+before_get "/edit/*" { |env| verify_request(env) }
 get "/edit/*" do |env|
 end
 
+before_get "/delete/*" { |env| verify_request(env) }
 get "/delete/*" do |env|
 end
 
-get "/show/*" do |env|
-  Markdown.to_html(File.read(get_file_path(env))) rescue env.response.status_code = 404
-end
-
+before_post "/*" { |env| verify_request(env) }
 post "/*" do |env|
   # .. create something ..
 end
 
+before_put "/*" { |env| verify_request(env) }
 put "/*" do |env|
   # .. replace something ..
 end
 
+before_delete "/*" { |env| verify_request(env) }
 delete "/*" do |env|
   # .. annihilate something ..
 end
