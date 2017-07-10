e1ad8cf0f2a2099186b622fa47ec3fe9ea5c3099
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-10 20:44:05 +0100

    Fix pages path set in the ACL

--- src/controllers/admin/pages.cr
+++ src/controllers/admin/pages.cr
@@ -2,22 +2,21 @@
 post "/admin/pages/*path" do |env|
   acl_permit! :write
   data = env.params.body
-  path = env.params.url["path"]
+  page = Wikicr::Page.new env.params.url["path"]
 
   Wikicr::ACL.read!
   # Set the new permissions
   groups = data["groups"].strip.split(",").map(&.strip)
   groups.delete ""
-  pp data
   if data["change"] == "read"
-    Wikicr::ACL.clear_permissions_of(path, Acl::Perm::Read)
-    Wikicr::ACL.add_permissions_to(path, groups, Acl::Perm::Read)
-    Wikicr::GUEST[path] = Acl::Perm::None unless groups.empty?
+    Wikicr::ACL.clear_permissions_of(page.real_url, Acl::Perm::Read)
+    Wikicr::ACL.add_permissions_to(page.real_url, groups, Acl::Perm::Read)
+    Wikicr::GUEST[page.real_url] = Acl::Perm::None unless groups.empty?
   elsif data["change"] == "write"
-    Wikicr::ACL.clear_permissions_of(path, Acl::Perm::Write)
-    Wikicr::ACL.add_permissions_to(path, groups, Acl::Perm::Write)
+    Wikicr::ACL.clear_permissions_of(page.real_url, Acl::Perm::Write)
+    Wikicr::ACL.add_permissions_to(page.real_url, groups, Acl::Perm::Write)
   end
   Wikicr::ACL.save!
 
-  env.redirect "/pages/#{path}"
+  env.redirect page.real_url
 end
