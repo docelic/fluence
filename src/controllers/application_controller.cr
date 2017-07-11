dd9f51667300fcb66a64e0d137764d7b142bd220
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-11 19:56:47 +0100

    Move authorisation macros into ApplicationController

--- src/controllers/application_controller.cr
+++ src/controllers/application_controller.cr
@@ -1,5 +1,28 @@
 class ApplicationController < Amber::Controller::Base
   LAYOUT = "application.slang"
+
+  macro user_signed_in?
+    session["username"]
+  end
+
+  macro current_user
+    %name = user_signed_in?
+    if (%name.nil?)
+      Wikicr::USERS.default || raise "User not signed in"
+    else
+      Wikicr::USERS.find(%name)
+    end
+  end
+
+  macro acl_permit!(perm)
+    if Wikicr::ACL.permitted?(current_user, request.path, Acl::PERM[{{perm}}])
+      puts "PERMITTED #{current_user} #{request.path} #{Acl::PERM[{{perm}}]}"
+    else
+      puts "NOT PERMITTED #{current_user} #{request.path} #{Acl::PERM[{{perm}}]}"
+      flash["danger"] = "You are not permitted to access this resource."
+      # redirect_to "/"
+    end
+  end
 end
 
 require "./**"
