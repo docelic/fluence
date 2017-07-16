6efd4cf07d56329c3b05b02bd2280878fd5675ee
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-16 15:31:37 +0100

    Factorize the page controller

--- src/controllers/pages_controller.cr
+++ src/controllers/pages_controller.cr
@@ -1,5 +1,3 @@
-
-
 class PagesController < ApplicationController
   # get /sitemap
   def sitemap
@@ -22,45 +20,64 @@ class PagesController < ApplicationController
     flash["danger"] = params.query["flash.danger"] if params.query["flash.danger"]?
     page = Wikicr::Page.new url: params.url["path"], read_title: true
     if (params.query["edit"]?) || !page.exists?
-      body = page.read rescue ""
-      flash["info"] = "The page #{page.url} does not exist yet." if !page.exists?
-      acl_permit! :write
-      render "edit.slang"
+      show_edit(page)
     else
-      body_html = Wikicr::Page::Markdown.to_html page.read, page, Wikicr::PAGES.load!
-      Wikicr::ACL.load!
-      groups_read = Wikicr::ACL.groups_having_any_access_to page.real_url, Acl::Perm::Read, true
-      groups_write = Wikicr::ACL.groups_having_any_access_to page.real_url, Acl::Perm::Write, true
-      render "show.slang"
+      show_show(page)
     end
+  end
+
+  private def show_edit(page)
+    body = page.read rescue ""
+    flash["info"] = "The page #{page.url} does not exist yet." if !page.exists?
+    acl_permit! :write
+    render "edit.slang"
+  end
+
+  private def show_show(page)
+    body_html = Wikicr::Page::Markdown.to_html page.read, page, Wikicr::PAGES.load!
+    Wikicr::ACL.load!
+    groups_read = Wikicr::ACL.groups_having_any_access_to page.real_url, Acl::Perm::Read, true
+    groups_write = Wikicr::ACL.groups_having_any_access_to page.real_url, Acl::Perm::Write, true
+    render "show.slang"
   end
 
   # post /pages/*path
   def update
     acl_permit! :write
-    pp params.body
     page = Wikicr::Page.new url: params.url["path"], read_title: true
     if params.body["rename"]?
-      if !params.body["new_path"]?.to_s.strip.empty?
-        # TODO: verify if the user can write on new_path
-        # TODO: if new_path do not begin with /, relative rename to the current path
-        page.rename current_user, params.body["new_path"]
-        flash["success"] = "The page #{page.url} has been moved to #{params.body["new_path"]}."
-        redirect_to "/pages/#{params.body["new_path"]}"
-      else
-        redirect_to page.real_url
-      end
+      update_rename(page)
     elsif (params.body["body"]?.to_s.empty?)
-      page.delete current_user rescue nil
-      flash["success"] = "The page #{page.url} has been deleted."
-      Wikicr::PAGES.transaction! { |index| index.delete page }
-      redirect_to "/pages/home"
+      update_delete(page)
     else
-      page.write current_user, params.body["body"]
-      page.read_title!
-      flash["success"] = "The page #{page.url} has been updated."
-      Wikicr::PAGES.transaction! { |index| index.add page }
+      update_edit(page)
+    end
+  end
+
+  private def update_rename(page)
+    if !params.body["new_path"]?.to_s.strip.empty?
+      # TODO: verify if the user can write on new_path
+      # TODO: if new_path do not begin with /, relative rename to the current path
+      page.rename current_user, params.body["new_path"]
+      flash["success"] = "The page #{page.url} has been moved to #{params.body["new_path"]}."
+      redirect_to "/pages/#{params.body["new_path"]}"
+    else
       redirect_to page.real_url
     end
+  end
+
+  private def update_delete(page)
+    page.delete current_user rescue nil
+    flash["success"] = "The page #{page.url} has been deleted."
+    Wikicr::PAGES.transaction! { |index| index.delete page }
+    redirect_to "/pages/home"
+  end
+
+  private def update_edit(page)
+    page.write current_user, params.body["body"]
+    page.read_title!
+    flash["success"] = "The page #{page.url} has been updated."
+    Wikicr::PAGES.transaction! { |index| index.add page }
+    redirect_to page.real_url
   end
 end
