bdf956b813b11bdfc353eb42c017e3f2bc7f0f8b
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-16 15:31:37 +0100

    Factorize the page controller

--- src/lib/options.cr
+++ src/lib/options.cr
@@ -2,8 +2,8 @@ class Wikicr::Options
   getter basedir : String
 
   def initialize
-    @basedir = File.expand_path(ENV["WIKI_DATA"]? || "data", Dir.current)
-    Dir.mkdir_p(@basedir) rescue nil
+    @basedir = File.expand_path ENV.fetch("WIKI_DATA", "data"), Dir.current
+    Dir.mkdir_p @basedir rescue nil
   end
 end
 
