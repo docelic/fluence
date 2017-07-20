433a911ba70d80bb7c396d7d2caa67efc95b6b29
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-20 18:14:01 +0100

    Move table of content and add special tags
    * Move Table of Content of Page into another module
    * Add special tag parser and render (to factorize)
    * Change \d160 into simple space for the titles

--- src/lib/files/page/index.cr
+++ src/lib/files/page/index.cr
@@ -1,106 +1,109 @@
 require "yaml"
+require "./table_of_content"
 
 # And Index is an object that associate a file with a lot of meta-data
 # like related url, the title, the table of content, ...
-class Wikicr::Page::Index < Lockable
-  class Entry
+struct Wikicr::Page
+  class Index < Lockable
+    class Entry
+      YAML.mapping(
+        path: String,  # path of the file /srv/wiki/data/xxx
+        url: String,   # real url of the page /pages/xxx
+        title: String, # Any title
+        slug: String,  # Exact matching title
+        toc: Page::Toc      )
+
+      def initialize(@path, @url, @title, toc : Bool = false)
+        @slug = Entry.title_to_slug title
+        @toc = toc ? Page::TableOfContent.toc(@path) : Page::TableOfContent::Toc.new
+      end
+
+      def self.title_to_slug(title : String) : String
+        title.downcase.gsub(/[^[:alnum:]]/, "-")
+      end
+    end
+
     YAML.mapping(
-      path: String,  # path of the file /srv/wiki/data/xxx
-      url: String,   # real url of the page /pages/xxx
-      title: String, # Any title
-      slug: String,  # Exact matching title
-      toc: Page::Toc    )
+      file: String,
+      entries: Hash(String, Entry) # path, entry
+    )
 
-    def initialize(@path, @url, @title, toc : Bool = false)
-      @slug = Entry.title_to_slug title
-      @toc = toc ? Page.toc(@path) : Page::Toc.new
-    end
-
-    def self.title_to_slug(title : String) : String
-      title.downcase.gsub(/[^[:alnum:]]/, "-")
-    end
-  end
-
-  YAML.mapping(
-    file: String,
-    entries: Hash(String, Entry) # path, entry
-  )
-
-  def initialize(@file : String)
-    @entries = {} of String => Entry
-  end
-
-  # Find a matching *text* into the Index
-  def find(text : String, context : Page)
-    found = find_by_title(text, context)
-    return {found.title, found.url} unless found.nil?
-    {text, "#{context.real_url_dirname}/#{text}"}
-  end
-
-  # Find the closest `Index`' `Entry` to *text* based on the entries title
-  # and searching for the closer url as possible to the context
-  private def find_by_title(text : String, context : Page) : Entry?
-    # exact_matched = @entries.select{|_, entry| entry.title == text }.values
-    # return choose_closer_url(exact_matched, context) unless exact_matched.empty?
-    slug_matched = @entries.select { |_, entry| entry.slug == Index::Entry.title_to_slug(text) }.values
-    return choose_closer_url(slug_matched, context) unless slug_matched.empty?
-    nil
-  end
-
-  # Find the url which is the closest as possible than the context url (start with the maxmimum common chars).
-  private def choose_closer_url(entries : Array(Entry), context : Page) : Entry
-    raise "Cannot handle empty array" if entries.empty?
-    entries.reduce { |lhs, rhs| Index.url_closeness(context.url, lhs.url) >= Index.url_closeness(context.url, rhs.url) ? lhs : rhs }
-  end
-
-  # Computes the amount of common chars at the beginning of each string
-  def self.url_closeness(from : String, to : String)
-    from.size.times do |i|
-      return i if from[i] != to[i]
-    end
-    return from.size
-  end
-
-  # Add a new `Entry`.
-  def [](page : Wikicr::Page) : Index::Entry
-    @entries[page.path]
-  end
-
-  # Add a new `Entry`.
-  def []?(page : Wikicr::Page) : Index::Entry?
-    @entries[page.path]?
-  end
-
-  # Add a new `Entry`.
-  def add(page : Wikicr::Page)
-    @entries[page.path] = Entry.new page.path, page.url, page.title, toc: true
-    self
-  end
-
-  # Remove an `Entry` from the `Index` based on its path.
-  def delete(page : Wikicr::Page)
-    @entries.delete page.path
-    self
-  end
-
-  # Replace the old Index using the state registrated into the *file*.
-  def load!
-    if File.exists?(@file) && (new_index = Index.read(@file) rescue nil)
-      @entries = new_index.entries
-      # @file = index.file
-    else
+    def initialize(@file : String)
       @entries = {} of String => Entry
     end
-    self
-  end
 
-  def self.read(file : String)
-    Index.from_yaml(File.read file)
-  end
+    # Find a matching *text* into the Index
+    def find(text : String, context : Page)
+      found = find_by_title(text, context)
+      return {found.title, found.url} unless found.nil?
+      {text, "#{context.real_url_dirname}/#{text}"}
+    end
 
-  # Save the current state into the file
-  def save!
-    File.write @file, self.to_yaml
-    self
+    # Find the closest `Index`' `Entry` to *text* based on the entries title
+    # and searching for the closer url as possible to the context
+    private def find_by_title(text : String, context : Page) : Entry?
+      # exact_matched = @entries.select{|_, entry| entry.title == text }.values
+      # return choose_closer_url(exact_matched, context) unless exact_matched.empty?
+      slug_matched = @entries.select { |_, entry| entry.slug == Index::Entry.title_to_slug(text) }.values
+      return choose_closer_url(slug_matched, context) unless slug_matched.empty?
+      nil
+    end
+
+    # Find the url which is the closest as possible than the context url (start with the maxmimum common chars).
+    private def choose_closer_url(entries : Array(Entry), context : Page) : Entry
+      raise "Cannot handle empty array" if entries.empty?
+      entries.reduce { |lhs, rhs| Index.url_closeness(context.url, lhs.url) >= Index.url_closeness(context.url, rhs.url) ? lhs : rhs }
+    end
+
+    # Computes the amount of common chars at the beginning of each string
+    def self.url_closeness(from : String, to : String)
+      from.size.times do |i|
+        return i if from[i] != to[i]
+      end
+      return from.size
+    end
+
+    # Add a new `Entry`.
+    def [](page : Wikicr::Page) : Index::Entry
+      @entries[page.path]
+    end
+
+    # Add a new `Entry`.
+    def []?(page : Wikicr::Page) : Index::Entry?
+      @entries[page.path]?
+    end
+
+    # Add a new `Entry`.
+    def add(page : Wikicr::Page)
+      @entries[page.path] = Entry.new page.path, page.url, page.title, toc: true
+      self
+    end
+
+    # Remove an `Entry` from the `Index` based on its path.
+    def delete(page : Wikicr::Page)
+      @entries.delete page.path
+      self
+    end
+
+    # Replace the old Index using the state registrated into the *file*.
+    def load!
+      if File.exists?(@file) && (new_index = Index.read(@file) rescue nil)
+        @entries = new_index.entries
+        # @file = index.file
+      else
+        @entries = {} of String => Entry
+      end
+      self
+    end
+
+    def self.read(file : String)
+      Index.from_yaml(File.read file)
+    end
+
+    # Save the current state into the file
+    def save!
+      File.write @file, self.to_yaml
+      self
+    end
   end
 end
--- src/lib/files/page/markdown/render.cr
+++ src/lib/files/page/markdown/render.cr
@@ -14,6 +14,11 @@ struct Wikicr::Page::Markdown
       @cursor = str.size + 1
     end
 
+    private def render_title(b, str)
+      # if the char 160 (space) is in a title, it is obviously a mistake.
+      render_full_line b, str.gsub(' ', ' ')
+    end
+
     private def render_quote(b, str)
       render_full_line b, str
     end
@@ -49,6 +54,25 @@ struct Wikicr::Page::Markdown
         # write the markdown link
         b << '[' << title << ']' << '(' << url << ')'
         @cursor = link_end + 2
+      else
+        render_full_line b, str
+      end
+    end
+
+    # Render an special_tag into the builder
+    # TODO: handle tag
+    # TODO: handle meta-data
+    private def render_special_tag(b : String::Builder, str : String, special_tag_begin : Int32)
+      text_begin = special_tag_begin + 2
+      # Search for the end (the content cannot contain '}' because it must be an "alnum" char)
+      if (special_tag_end = str.index('}', text_begin)) && str[special_tag_end + 1] == '}'
+        text_end = special_tag_end - 1
+        text = str[text_begin..text_end]
+        # render before the special_tag
+        b << str[@cursor..(special_tag_begin - 1)] unless special_tag_begin == 0
+        # write the markdown link
+        b << "**Special tag party:** *#{text}*\n"
+        @cursor = special_tag_end + 2
       else
         render_full_line b, str
       end
