1c4a520c85a61e11b889344101c4328d8570548a
Arthur Poulet <arthur.poulet@mailoo.org>
2017-07-20 23:12:12 +0100

    Improve toc display

--- src/controllers/helpers/page.cr
+++ src/controllers/helpers/page.cr
@@ -7,8 +7,8 @@ module Wikicr::Helpers::Page
   end
 
   # TODO: move that
-  def create_toc_line(line, current_id)
-    "<li><a href=\"#section-#{current_id + 1}\">#{line}</a></li>"
+  def create_toc_line(line, current_id, ends = true)
+    "<li><a href=\"#section-#{current_id + 1}\">#{line}</a>#{ends ? "</li>" : nil}\n"
   end
 
   # TODO: move that
@@ -17,12 +17,15 @@ module Wikicr::Helpers::Page
     current_entry = index_entry[current_id]
     current_head = current_entry[0]
     current_head_value = current_entry[1]
+    next_entry = index_entry[current_id + 1]?
+    next_head = next_entry ? next_entry[0] : 7
+    close_li = next_head <= current_head
     if current_head > last_head
-      b << "<ul>" << create_toc_line(current_head_value, current_id)
+      b << "<ol>\n" << create_toc_line(current_head_value, current_id, close_li)
     elsif current_head < last_head
-      b << "</ul>" << create_toc_line(current_head_value, current_id)
+      b << "</ol></li>\n" << create_toc_line(current_head_value, current_id, close_li)
     else
-      b << create_toc_line(current_head_value, current_id)
+      b << create_toc_line(current_head_value, current_id, close_li)
     end
     return add_toc_level(b, index_entry, current_id + 1, current_head)
   end
