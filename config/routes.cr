0be47cfe79f3532d4a24e8e777f40f089331c36d
Davor Ocelic <docelic@crystallabs.io>
2018-08-04 22:35:59 +0200

    Remove /p/ shorthands; they would require double ACLs

--- config/routes.cr
+++ config/routes.cr
@@ -22,24 +22,23 @@ class Router
 end
 
 Router.get "/", HomeController, :index
+
 Router.get "/sitemap", PagesController, :sitemap
 
-# /pages/
 Router.get "/pages/search", PagesController, :search
+
 Router.get "/pages/*path", PagesController, :show
 Router.post "/pages/*path", PagesController, :update
-# /p/ shorthand
-Router.get "/p/search", PagesController, :search
-Router.get "/p/*path", PagesController, :show
-Router.post "/p/*path", PagesController, :update
 
 Router.get "/users/login", UsersController, :login
 Router.post "/users/login", UsersController, :login_validates
 Router.get "/users/register", UsersController, :register
 Router.post "/users/register", UsersController, :register_validates
+
 Router.get "/admin/users", AdminController, :users_show
 Router.post "/admin/users/create", AdminController, :user_create
 Router.post "/admin/users/delete", AdminController, :user_delete
+
 Router.get "/admin/acls", AdminController, :acls_show
 Router.post "/admin/acls/update", AdminController, :acl_update
 Router.post "/admin/acls/create", AdminController, :acl_create
