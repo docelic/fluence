cab46f40bb8aaf8d2af81632930db6a2f1fc9e3d
Davor Ocelic <docelic@crystallabs.io>
2018-08-22 22:43:00 +0200

    Start moving all files to space-based indentation

--- config/options.cr
+++ config/options.cr
@@ -2,39 +2,39 @@ module Fluence
   class Options
     def initialize
 
-			# Brand name, full name (including optional credit to Fluence Wiki), and logo.
-			@brand = "Fluence"
-			@brand_info = "#{@brand} - Fluence Wiki"
-			@brand_logo = "/logo.png"
+      # Brand name, full name (including optional credit to Fluence Wiki), and logo.
+      @brand = "Fluence"
+      @brand_info = "#{@brand} - Fluence Wiki"
+      @brand_logo = "/logo.png"
 
-			# Location of data/ and meta/ directories. Defaults to $PWD/{data,meta}
+      # Location of data/ and meta/ directories. Defaults to $PWD/{data,meta}
       @datadir = ::File.expand_path ENV.fetch("FLUENCE_DATADIR", "data"), Dir.current
       @metadir = ::File.expand_path ENV.fetch("FLUENCE_METADIR", "meta"), Dir.current
 
-			# Visible part of URL through which pages are accessed, e.g. /pages/my_page
-			@pages_prefix = "/pages"
-			# Start page / homepage. Defaults to /pages/home
-			@homepage = ::File.join @pages_prefix, "home"
+      # Visible part of URL through which pages are accessed, e.g. /pages/my_page
+      @pages_prefix = "/pages"
+      # Start page / homepage. Defaults to /pages/home
+      @homepage = ::File.join @pages_prefix, "home"
 
-			# Visible part of URL through which media is accessed, e.g. /media/my_page/my_file1.pdf
-			@media_prefix = "/media"
+      # Visible part of URL through which media is accessed, e.g. /media/my_page/my_file1.pdf
+      @media_prefix = "/media"
 
-			# Location of users and admin interfaces
-			@users_prefix = "/users"
-			@admin_prefix = "/admin"
+      # Location of users and admin interfaces
+      @users_prefix = "/users"
+      @admin_prefix = "/admin"
 
-			# Recursion limit for any recursive functions
-			@recursion_limit = 1000
+      # Recursion limit for any recursive functions
+      @recursion_limit = 1000
 
-			# Do all, or only new and/or only empty pages open in edit mode by default?
-			# By default, only new pages open in edit mode; existing and empty pages open in view mode.
-			@open_in_edit = false
-			@open_new_in_edit = true
-			@open_empty_in_edit = false
+      # Do all, or only new and/or only empty pages open in edit mode by default?
+      # By default, only new pages open in edit mode; existing and empty pages open in view mode.
+      @open_in_edit = false
+      @open_new_in_edit = true
+      @open_empty_in_edit = false
 
-			#
-			# No need to configure anything below this point.
-			#
+      #
+      # No need to configure anything below this point.
+      #
 
       Dir.mkdir_p @datadir
       Dir.mkdir_p @metadir
